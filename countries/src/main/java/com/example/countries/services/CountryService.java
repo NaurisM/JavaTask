ed0926d095487889b2aeaae06335ae4b9a08f94f
NaurisM <65161296+NaurisM@users.noreply.github.com>
2021-10-12 18:49:36 +0300

    add type

--- countries/src/main/java/com/example/countries/services/CountryService.java
+++ countries/src/main/java/com/example/countries/services/CountryService.java
@@ -24,29 +24,28 @@ public class CountryService implements ICountryService {
 
     public List<String> sortListByArea() {
         List<Country> countryList = getAllCountries();
-        List<String > areaList = new ArrayList();
-        Collections.sort(countryList,
-                Comparator.comparingInt(Country::getArea).reversed());
+        List<String> areaList = new ArrayList<String>();
+        Collections.sort(countryList, Comparator.comparingInt(Country::getArea).reversed());
         countryList.subList(0, 10).forEach(country -> areaList.add(country.getName() + " : " + country.getArea()));
         return areaList;
     }
 
     public List<String> sortListByPopulation() {
         List<Country> countryList = getAllCountries();
-        List<String > populationList = new ArrayList();
-        Collections.sort(countryList,
-                Comparator.comparingInt(Country::getPopulation).reversed());
-        countryList.subList(0, 10).forEach(country -> populationList.add(country.getName() + " : " + country.getPopulation()));
+        List<String> populationList = new ArrayList<String>();
+        Collections.sort(countryList, Comparator.comparingInt(Country::getPopulation).reversed());
+        countryList.subList(0, 10)
+                .forEach(country -> populationList.add(country.getName() + " : " + country.getPopulation()));
         return populationList;
     }
 
     public List<String> sortListByDensity() {
-        List<Country> countryList= getAllCountries();
-        List<String > densityList = new ArrayList();
+        List<Country> countryList = getAllCountries();
+        List<String> densityList = new ArrayList<String>();
         DecimalFormat df = new DecimalFormat("#.#");
-        Collections.sort(countryList,
-                Comparator.comparingDouble(Country::calculateDensity).reversed());
-        countryList.subList(1, 11).forEach(country -> densityList.add(country.getName() + " : " + df.format(country.calculateDensity())));
+        Collections.sort(countryList, Comparator.comparingDouble(Country::calculateDensity).reversed());
+        countryList.subList(1, 11)
+                .forEach(country -> densityList.add(country.getName() + " : " + df.format(country.calculateDensity())));
         return densityList;
     }
 }
